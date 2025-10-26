665e661d428222d203dfd24fa2e3f5f6116c5ec8
matt_chandran <mattchandran23@gmail.com>
2025-10-26 00:56:17 -0400

    Condensed code for initializing the library markers

--- src/components/Spot.tsx
+++ src/components/Spot.tsx
@@ -25,69 +25,38 @@ const Spot = () => {
       attribution: "&copy; OpenStreetMap contributors",
     }).addTo(map);
 
-    //array for Markers
-    const markers = [];
+    //array for library locations
+    const libraryLocations: { name: string; coords: [number, number] }[] = [
+      { name: "OSU is here", coords: [39.99955,  -83.01231] },
+      { name: "Thompson Library", coords: [39.99923, -83.01489] },
+      { name: "Health Sciences Library", coords: [39.99477, -83.01710] },
+      { name: "18th Ave Library", coords: [40.00163,  -83.01337] },
+      { name: "Geology Library", coords: [39.99839,  -83.01179] },
+      { name: "Architecture Library", coords: [40.00374,  -83.01640] },
+      { name: "Fine Arts Library", coords: [40.00056,  -83.00897] },
+      { name: "FAES Library", coords: [40.00511,  -83.02716] },
+      { name: "Law Library", coords: [39.99630,  -83.00827] },
+      { name: "Veterinary Medicine Library", coords: [39.99945,  -83.02645] },
+    ];
 
-    //osu
-    const osuMarker = L.marker([39.99955,  -83.01231])
-    .addTo(map).bindPopup("OSU is here!")
-    .openPopup();
-    
-    markers.push(osuMarker);
+    //array of markers
+    const markers: L.Marker[] = []
+    libraryLocations.forEach(lib => {
+      //set marker for each location
+      const marker = L.marker(lib.coords).addTo(map).bindPopup(`${lib.name}!`);
+      markers.push(marker);
 
-    //thompson
-    const thompMarker = L.marker([39.99923,  -83.01489])
-    .addTo(map).bindPopup("Thompson!");
+      //action listener:
+      marker.on("click", (e: L.LeafletMouseEvent) => {
+        marker.setIcon(redIcon);
+      });
 
-    markers.push(thompMarker);
+    });
 
-    //HSL
-    const hslMarker = L.marker([39.99477,  -83.01710])
-    .addTo(map).bindPopup("Health Sciences Library!");
 
-    markers.push(hslMarker);
 
-    //18th Ave Library
-    const aveMarker = L.marker([40.00163,  -83.01337])
-    .addTo(map).bindPopup("18th Ave Library!");
-
-    markers.push(aveMarker);
-
-    //Geology Library
-    const geoMarker = L.marker([39.99839,  -83.01179])
-    .addTo(map).bindPopup("Geology Library!");
-
-    markers.push(geoMarker);
-
-    //Architecture Library
-    const archMarker = L.marker([40.00374,  -83.01640])
-    .addTo(map).bindPopup("Architecture Library!");
-
-    markers.push(archMarker);
-
-    //Fine arts Library
-    const artsMarker = L.marker([40.00056,  -83.00897])
-    .addTo(map).bindPopup("Fine Arts Library!");
-
-    markers.push(artsMarker);
-
-    //FAES Library
-    const faesMarker = L.marker([40.00511,  -83.02716])
-    .addTo(map).bindPopup("FAES Library!");
-
-    markers.push(faesMarker);
-    
-    //Law Library
-    const lawMarker = L.marker([39.99630,  -83.00827])
-    .addTo(map).bindPopup("Law Library!");
-
-    markers.push(lawMarker);
-
-    //Veterinary Medicine Library
-    const vetMarker = L.marker([39.99945,  -83.02645])
-    .addTo(map).bindPopup("Veterinary Medicine Library!");
-
-    markers.push(vetMarker);
+    //open message "OSU is here!"
+    markers[0].openPopup();
 
     for (let i = 0; i < markers.length; i++) {
       
