08febe2c4f09f19ca63453de03a4ec5f94aa5fbd
matt_chandran <mattchandran23@gmail.com>
2025-10-25 20:20:31 -0400

    Added all library markers

--- src/components/Spot.tsx
+++ src/components/Spot.tsx
@@ -24,11 +24,62 @@ const Spot = () => {
       attribution: "&copy; OpenStreetMap contributors",
     }).addTo(map);
 
-    L.marker([40.441, -82.9]).addTo(map).bindPopup("OSU is here!").openPopup();
+    //osu
+    const osuMarker = L.marker([39.99955,  -83.01231])
+    .addTo(map).bindPopup("OSU is here!")
+    .openPopup();
+
     /*L.marker([95, 30], { icon: redIcon })
       .addTo(map)
       .bindPopup("Another spot!")
-      .openPopup(); */
+   */
+
+    //thompson
+    const thompMarker = L.marker([39.99923,  -83.01489])
+    .addTo(map).bindPopup("Thompson!");
+
+
+    //HSL
+    const hslMarker = L.marker([39.99477,  -83.01710])
+    .addTo(map).bindPopup("Health Sciences Library!");
+
+
+    //18th Ave Library
+    const aveMarker = L.marker([40.00163,  -83.01337])
+    .addTo(map).bindPopup("18th Ave Library!");
+
+    //Geology Library
+    const geoMarker = L.marker([39.99839,  -83.01179])
+    .addTo(map).bindPopup("Geology Library!");
+
+    //Architecture Library
+    const archMarker = L.marker([40.00374,  -83.01640])
+    .addTo(map).bindPopup("Architecture Library!");
+
+    //Fine arts Library
+    const artsMarker = L.marker([40.00056,  -83.00897])
+    .addTo(map).bindPopup("Fine Arts Library!");
+
+    //FAES Library
+    const faesMarker = L.marker([40.00511,  -83.02716])
+    .addTo(map).bindPopup("FAES Library!");
+    
+    //Law Library
+    const lawMarker = L.marker([39.99630,  -83.00827])
+    .addTo(map).bindPopup("Law Library!");
+
+    //Veterinary Medicine Library
+    const vetMarker = L.marker([39.99945,  -83.02645])
+    .addTo(map).bindPopup("Vet Library!");
+
+    //action listener to find coordinates easier
+    map.on("click", (e: L.LeafletMouseEvent) => {
+      const { lat, lng } = e.latlng;
+      L.popup()
+        .setLatLng(e.latlng)
+        .setContent(`Coordinates:<br>Latitude: ${lat.toFixed(5)}<br>Longitude: ${lng.toFixed(5)}`)
+        .openOn(map);
+    });
 
     // Optional cleanup
     return () => {
